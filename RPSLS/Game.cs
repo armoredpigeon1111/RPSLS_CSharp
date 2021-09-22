ae9c46b93adf19a90b7eaffe580a46659b7bbba0
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-09-22 16:57:28 -0400

    Added space to DisplayRules output

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -62,7 +62,7 @@ namespace RPSLS
             Console.WriteLine("\nEach player choses one gesture from the list\n" +
                 " Rock crushes Scissors\n Scissors cuts Paper\n Paper covers Rock" +
                 "\n Rock crushes Lizard\n Lizard poisons Spock\n Spock smashes Scissors\n" +
-                " Scissors decapitates Lizard\n Lizard eats Paper\n Paper disproves Spock\n Spock vaporizes Rock");
+                " Scissors decapitates Lizard\n Lizard eats Paper\n Paper disproves Spock\n Spock vaporizes Rock\n");
 
         }
 
