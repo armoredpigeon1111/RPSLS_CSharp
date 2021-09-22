57d8a9b2b37670e3ec4b2cae9c53fd7905f2b332
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-09-22 12:37:10 -0400

    Add project files.

---
+++ RPSLS/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Game
+    {
+        public void RunGame(){
+            WelcomeMessage();
+            DisplayRules();
+        }
+
+        public void WelcomeMessage() {
+            Console.WriteLine("Welcome to Rock, Paper, Scissors, Lizard, Spock!");
+        }
+
+        public void DisplayRules()
+        {
+            Console.WriteLine("\nEach player choses one gesture from the list\n" +
+                " Rock crushes Scissors\n Scissors cuts Paper\n Paper covers Rock" +
+                "\n Rock crushes Lizard\n Lizard poisons Spock\n Spock smashes Scissors\n" +
+                " Scissors decapitates Lizard\n Lizard eats Paper\n Paper disproves Spock\n Spock vaporizes Rock");
+
+        }
+
+    }
+}
---
+++ RPSLS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RPSLS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game1 = new Game();
+            game1.RunGame();
+        }
+    }
+}
